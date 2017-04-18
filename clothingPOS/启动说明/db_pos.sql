73e7c4fa7932b2eae4cf06f1b25c2b54568cc10b
jodenhe <824923282@qq.com>
2017-04-19 00:02:19 +0800

    同步资源！

--- clothingPOS/启动说明/db_pos.sql
+++ clothingPOS/启动说明/db_pos.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : local
+Source Server         : mysql
 Source Server Version : 50717
 Source Host           : localhost:3306
 Source Database       : db_pos
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-03-23 23:39:25
+Date: 2017-04-18 23:40:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,14 +24,16 @@ CREATE TABLE `brand` (
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '名称，非空',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
   `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态,"0"为下架，"1"为上架，"2"为删除废弃',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of brand
 -- ----------------------------
-INSERT INTO `brand` VALUES ('1', '李宁', null, '2017-03-19 12:59:07');
-INSERT INTO `brand` VALUES ('2', 'ANTA', null, '2017-03-19 13:00:02');
+INSERT INTO `brand` VALUES ('1', '李宁', null, '2017-03-19 12:59:07', '0');
+INSERT INTO `brand` VALUES ('2', 'ANTA', null, '2017-03-19 13:00:02', '0');
+INSERT INTO `brand` VALUES ('3', 'test', 'adad11', '2017-03-27 22:23:54', '0');
 
 -- ----------------------------
 -- Table structure for category
@@ -43,19 +45,22 @@ CREATE TABLE `category` (
   `pId` bigint(20) DEFAULT NULL COMMENT '父类别，参照本表的id',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
   `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态,"0"为下架，"1"为上架，"2"为删除废弃',
   PRIMARY KEY (`id`),
   KEY `FK_parentId` (`pId`),
   CONSTRAINT `FK_parentId` FOREIGN KEY (`pId`) REFERENCES `category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of category
 -- ----------------------------
-INSERT INTO `category` VALUES ('14', '服装', null, '测试', '2017-03-18 21:58:00');
-INSERT INTO `category` VALUES ('15', '上衣2', '14', '测试', '2017-03-23 15:53:59');
-INSERT INTO `category` VALUES ('17', '皮衣', '15', '测试', '2017-03-18 22:01:48');
-INSERT INTO `category` VALUES ('18', '皮衣的子类1', '17', '测试', '2017-03-23 15:49:37');
-INSERT INTO `category` VALUES ('28', 'T恤', '15', '的规范地方', '2017-03-23 15:54:37');
+INSERT INTO `category` VALUES ('14', '服装', null, '测试', '2017-03-27 22:17:15', '0');
+INSERT INTO `category` VALUES ('15', '上衣', '14', '测试', '2017-03-24 10:26:57', '0');
+INSERT INTO `category` VALUES ('17', '皮衣', '15', '测试', '2017-03-18 22:01:48', '0');
+INSERT INTO `category` VALUES ('28', 'T恤', '15', '11221', '2017-04-09 21:26:40', '0');
+INSERT INTO `category` VALUES ('29', '鞋子', null, null, '2017-04-09 11:29:23', '0');
+INSERT INTO `category` VALUES ('31', '12', null, null, '2017-04-11 22:04:18', '0');
+INSERT INTO `category` VALUES ('32', '343', null, null, '2017-04-11 22:12:06', '0');
 
 -- ----------------------------
 -- Table structure for color
@@ -66,13 +71,15 @@ CREATE TABLE `color` (
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '名称，非空',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
   `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态,"0"为下架，"1"为上架，"2"为删除废弃',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of color
 -- ----------------------------
-INSERT INTO `color` VALUES ('1', '黄色', '', '2017-03-18 10:28:35');
+INSERT INTO `color` VALUES ('1', '黄色', '萨达是', '2017-03-27 22:35:58', '0');
+INSERT INTO `color` VALUES ('2', '蓝色', '阿达是', '2017-03-27 22:38:09', '0');
 
 -- ----------------------------
 -- Table structure for goods
@@ -90,20 +97,29 @@ CREATE TABLE `goods` (
   `price` decimal(10,2) DEFAULT '0.00' COMMENT '售价',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
   `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态,"0"为下架，"1"为上架，"2"为删除废弃',
   PRIMARY KEY (`id`),
   KEY `FK_goods_color` (`colorId`),
   KEY `FK_goods_categoryId` (`categoryId`),
   KEY `FK_goods_brandId` (`brandId`),
   KEY `FK_goods_sizeId` (`sizeId`),
+  KEY `barcode` (`barcode`),
   CONSTRAINT `FK_goods_brandId` FOREIGN KEY (`brandId`) REFERENCES `brand` (`id`),
   CONSTRAINT `FK_goods_categoryId` FOREIGN KEY (`categoryId`) REFERENCES `category` (`id`),
   CONSTRAINT `FK_goods_color` FOREIGN KEY (`colorId`) REFERENCES `color` (`id`),
   CONSTRAINT `FK_goods_sizeId` FOREIGN KEY (`sizeId`) REFERENCES `size` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
+INSERT INTO `goods` VALUES ('2', '28', '2', '3', '4', '20170325015835', '测试24', '阿三大婶a', '261.00', '1111\najfdasf', '2017-04-11 22:00:40', '0');
+INSERT INTO `goods` VALUES ('8', '28', '2', '3', '1', '20170411211559', '大法师2', '士大夫是', '12.00', null, '2017-04-11 22:01:28', '0');
+INSERT INTO `goods` VALUES ('9', '28', '1', '1', '5', '20170411220201', 'dsfsd', null, '10.00', 'safds', '2017-04-11 22:02:01', '0');
+INSERT INTO `goods` VALUES ('10', '28', '1', '1', '1', '20170411221135', 'rtyr', null, '4.00', null, '2017-04-11 22:11:35', '0');
+INSERT INTO `goods` VALUES ('11', '28', '1', '1', '1', '20170411221145', 'rtyr34', null, '4.00', null, '2017-04-11 22:11:45', '0');
+INSERT INTO `goods` VALUES ('12', '28', '1', '1', '1', '20170411221930', 'fgy', null, '0.00', null, '2017-04-11 22:19:30', '0');
+INSERT INTO `goods` VALUES ('13', '28', '1', '1', '1', '20170411222447', 'wrwre', null, '0.00', null, '2017-04-11 22:24:47', '0');
 
 -- ----------------------------
 -- Table structure for inventory
@@ -135,6 +151,7 @@ CREATE TABLE `member` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id标识',
   `shopId` bigint(20) NOT NULL COMMENT '店铺id，参照shop表',
   `operatorId` bigint(20) NOT NULL COMMENT '操作员id，参照user表',
+  `customerNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '会员编号，0000默认为散客',
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '姓名',
   `sex` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '性别',
   `telephone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '手机',
@@ -148,12 +165,40 @@ CREATE TABLE `member` (
   PRIMARY KEY (`id`),
   KEY `FK_member_shopId` (`shopId`),
   KEY `FK_member_operatorId` (`operatorId`),
+  KEY `customerNo` (`customerNo`),
   CONSTRAINT `FK_member_shopId` FOREIGN KEY (`shopId`) REFERENCES `shop` (`id`),
   CONSTRAINT `FK_members_operatorId` FOREIGN KEY (`operatorId`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of member
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for payment
+-- ----------------------------
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id标识',
+  `paymentNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '支付编号',
+  `saleOrderNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '订单号，参照sale_order表',
+  `amount` decimal(10,2) NOT NULL COMMENT '支付金额',
+  `payType` tinyint(4) NOT NULL DEFAULT '0' COMMENT '支付方式，0代表现金支付',
+  `payTransactionNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '支付交易号，现金支付为空',
+  `payDateTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '支付时间',
+  `operatorId` bigint(20) NOT NULL COMMENT '操作员id，参照user表',
+  `shopId` bigint(20) NOT NULL COMMENT '店铺id',
+  PRIMARY KEY (`id`),
+  KEY `FK_p_saleOrderNo` (`saleOrderNo`),
+  KEY `FK_p_operatorId` (`operatorId`),
+  KEY `FK_p_shopId` (`shopId`),
+  CONSTRAINT `FK_p_operatorId` FOREIGN KEY (`operatorId`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_p_saleOrderNo` FOREIGN KEY (`saleOrderNo`) REFERENCES `sale_order` (`saleOrderNo`),
+  CONSTRAINT `FK_p_shopId` FOREIGN KEY (`shopId`) REFERENCES `shop` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of payment
 -- ----------------------------
 
 -- ----------------------------
@@ -207,29 +252,100 @@ CREATE TABLE `role_permission` (
 INSERT INTO `role_permission` VALUES ('1', '2', '1');
 
 -- ----------------------------
--- Table structure for sales
+-- Table structure for sale_order
 -- ----------------------------
-DROP TABLE IF EXISTS `sales`;
-CREATE TABLE `sales` (
+DROP TABLE IF EXISTS `sale_order`;
+CREATE TABLE `sale_order` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id标识',
-  `shopId` bigint(20) NOT NULL COMMENT '店铺id，参照shop表',
-  `goodsId` bigint(20) NOT NULL COMMENT '商品id，参照goods表',
-  `operatorId` bigint(20) NOT NULL COMMENT '操作员id，参照user表',
-  `quantity` int(20) NOT NULL DEFAULT '0' COMMENT '销售数量',
-  `salesAmount` decimal(20,2) NOT NULL DEFAULT '0.00' COMMENT '销售金额',
-  `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
-  `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `saleOrderNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '订单编号',
+  `customerNo` varchar(20) COLLATE utf8_unicode_ci NOT NULL COMMENT '客户编号',
+  `saleDateTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '下单时间',
+  `total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `operatorId` bigint(20) NOT NULL,
+  `shopId` bigint(20) NOT NULL COMMENT '店铺id',
   PRIMARY KEY (`id`),
-  KEY `FK_sales_shopId` (`shopId`),
-  KEY `FK_sales_goodsId` (`goodsId`),
-  KEY `FK_sales_operatorId` (`operatorId`),
-  CONSTRAINT `FK_sales_goodsId` FOREIGN KEY (`goodsId`) REFERENCES `goods` (`id`),
-  CONSTRAINT `FK_sales_operatorId` FOREIGN KEY (`operatorId`) REFERENCES `user` (`id`),
-  CONSTRAINT `FK_sales_shopId` FOREIGN KEY (`shopId`) REFERENCES `shop` (`id`)
+  KEY `FK_so_customerNo` (`customerNo`),
+  KEY `FK_so_operatorId` (`operatorId`),
+  KEY `saleOrderNo` (`saleOrderNo`),
+  KEY `FK_so_shopId` (`shopId`),
+  CONSTRAINT `FK_so_customerNo` FOREIGN KEY (`customerNo`) REFERENCES `member` (`customerNo`),
+  CONSTRAINT `FK_so_operatorId` FOREIGN KEY (`operatorId`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_so_shopId` FOREIGN KEY (`shopId`) REFERENCES `shop` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
--- Records of sales
+-- Records of sale_order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sale_order_item
+-- ----------------------------
+DROP TABLE IF EXISTS `sale_order_item`;
+CREATE TABLE `sale_order_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id标记',
+  `saleOrderNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '订单号，参照sale_order表订单编号',
+  `itemId` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '商品编号，即条形码，参照goods表',
+  `quantity` int(11) NOT NULL DEFAULT '1' COMMENT '数量',
+  `salePrice` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '单价',
+  `subTotal` decimal(10,2) NOT NULL COMMENT '小计',
+  PRIMARY KEY (`id`),
+  KEY `FK_soi_saleOrderNo` (`saleOrderNo`),
+  KEY `FK_soi_itemId` (`itemId`),
+  CONSTRAINT `FK_soi_itemId` FOREIGN KEY (`itemId`) REFERENCES `goods` (`barcode`),
+  CONSTRAINT `FK_soi_saleOrderNo` FOREIGN KEY (`saleOrderNo`) REFERENCES `sale_order` (`saleOrderNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of sale_order_item
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sale_reject_order
+-- ----------------------------
+DROP TABLE IF EXISTS `sale_reject_order`;
+CREATE TABLE `sale_reject_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id标记',
+  `rejectNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '退货单号',
+  `rejectOrderNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '源订单号，参照sale_order表',
+  `rejectDateTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '退货时间',
+  `total` decimal(10,2) NOT NULL COMMENT '退货总金额',
+  `operatorId` bigint(20) NOT NULL COMMENT '操作员id，参照user表',
+  `shopId` bigint(20) NOT NULL COMMENT '店铺id',
+  PRIMARY KEY (`id`),
+  KEY `FK_sro_rejectOrderNo` (`rejectOrderNo`),
+  KEY `FK_sro_operatorId` (`operatorId`),
+  KEY `rejectNo` (`rejectNo`),
+  KEY `FK_sro_shopId` (`shopId`),
+  CONSTRAINT `FK_sro_operatorId` FOREIGN KEY (`operatorId`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_sro_rejectOrderNo` FOREIGN KEY (`rejectOrderNo`) REFERENCES `sale_order` (`saleOrderNo`),
+  CONSTRAINT `FK_sro_shopId` FOREIGN KEY (`shopId`) REFERENCES `shop` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of sale_reject_order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sale_reject_order_item
+-- ----------------------------
+DROP TABLE IF EXISTS `sale_reject_order_item`;
+CREATE TABLE `sale_reject_order_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id标识',
+  `rejectNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '退货订单号，参照sale_reject_order表',
+  `itemId` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '商品编号，条形码，参照goods表',
+  `quantity` int(11) NOT NULL,
+  `rejectPrice` decimal(10,2) NOT NULL COMMENT '退货单价',
+  `subTotal` decimal(10,2) NOT NULL COMMENT '小计',
+  `rejectReason` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '退货理由',
+  PRIMARY KEY (`id`),
+  KEY `FK_sroi_rejectNo` (`rejectNo`),
+  KEY `FK_sroi_itemId` (`itemId`),
+  CONSTRAINT `FK_sroi_itemId` FOREIGN KEY (`itemId`) REFERENCES `goods` (`barcode`),
+  CONSTRAINT `FK_sroi_rejectNo` FOREIGN KEY (`rejectNo`) REFERENCES `sale_reject_order` (`rejectNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records of sale_reject_order_item
 -- ----------------------------
 
 -- ----------------------------
@@ -244,6 +360,7 @@ CREATE TABLE `shop` (
   `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '住址',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
   `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态,"0"为下架，"1"为上架，"2"为删除废弃',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -261,16 +378,22 @@ CREATE TABLE `size` (
   `type` varchar(40) COLLATE utf8_unicode_ci NOT NULL COMMENT '尺码所属的类型（例如上衣的尺码，裤子，鞋子等）',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述',
   `createTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态,"0"为下架，"1"为上架，"2"为删除废弃',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of size
 -- ----------------------------
-INSERT INTO `size` VALUES ('1', 'XL', '衣服', null, '2017-03-23 23:24:54');
-INSERT INTO `size` VALUES ('2', 'L', '衣服', null, '2017-03-23 23:24:56');
-INSERT INTO `size` VALUES ('3', 'S', '衣服', null, '2017-03-23 23:24:57');
-INSERT INTO `size` VALUES ('4', 'M', '衣服', null, '2017-03-23 23:24:59');
+INSERT INTO `size` VALUES ('1', 'XL', '衣服', null, '2017-03-23 23:24:54', '0');
+INSERT INTO `size` VALUES ('2', 'L', '衣服', null, '2017-03-24 22:36:21', '0');
+INSERT INTO `size` VALUES ('3', 'S', '衣服', null, '2017-03-23 23:24:57', '0');
+INSERT INTO `size` VALUES ('4', 'M', '衣服', null, '2017-03-23 23:24:59', '0');
+INSERT INTO `size` VALUES ('5', '30', '鞋子', '阿凡萨达', '2017-03-27 23:41:25', '0');
+INSERT INTO `size` VALUES ('6', 'ml', '衣服', null, '2017-04-08 18:02:20', '0');
+INSERT INTO `size` VALUES ('7', 'snad', '衣服', null, '2017-04-08 18:05:57', '0');
+INSERT INTO `size` VALUES ('8', 'a', '衣服', null, '2017-04-08 18:28:24', '0');
+INSERT INTO `size` VALUES ('9', 'ada', '鞋子', 'aasd', '2017-04-08 18:31:12', '0');
 
 -- ----------------------------
 -- Table structure for user
@@ -317,7 +440,10 @@ CREATE TABLE `warehouse` (
   `telephone` varchar(40) COLLATE utf8_unicode_ci NOT NULL COMMENT '联系电话',
   `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '仓库地址',
   `script` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '描述，可为空',
-  PRIMARY KEY (`id`)
+  `shopId` bigint(20) NOT NULL COMMENT '店铺id',
+  PRIMARY KEY (`id`),
+  KEY `FK_w_shopId` (`shopId`),
+  CONSTRAINT `FK_w_shopId` FOREIGN KEY (`shopId`) REFERENCES `shop` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
