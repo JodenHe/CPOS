ecfb26c5ba057ace70ea6d4638b25894b95e80a1
jodenhe <824923282@qq.com>
2017-04-22 15:05:41 +0800

    实现双击添加商品到销售明细表功能

--- clothingPOS/WebRoot/resources/js/index2.js
+++ clothingPOS/WebRoot/resources/js/index2.js
@@ -1,12 +1,13 @@
 $(function() {
 	setInterval("showTime()",1000);
 	$('.selectCombo').comboSelect();
+
     modelCenter();//模态库居中
+    inintSaleItemTable();//初始化销售明细表
 });
 
 /* center modal */ 
 function modelCenter(){
-
     function centerModals() { 
         $('#saleModal').each(function(i) { 
         var $clone = $(this).clone().css('display', 'block').appendTo('body'); var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2); 
@@ -14,16 +15,16 @@ function modelCenter(){
         $clone.remove(); 
         $(this).find('.modal-content').css("margin-top", top); 
     });
-    $('#goodsModal').each(function(i) { 
+   /* $('#goodsModal').each(function(i) { 
         var $clone = $(this).clone().css('display', 'block').appendTo('body'); 
         var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2); 
         top = top > 0 ? top : 0; 
         $clone.remove(); 
         $(this).find('.modal-content').css("margin-top", top); 
-    });  
+    });  */
     } 
     $('#saleModal').on('show.bs.modal', centerModals); 
-    $('#goodsModal').on('show.bs.modal', centerModals); 
+    // $('#goodsModal').on('show.bs.modal', centerModals); 
     $(window).on('resize', centerModals);
     
 }
@@ -70,6 +71,27 @@ function checkNum(num){
     return num;
 }
 
+//初始化明细表
+function inintSaleItemTable(){
+    $("#sale-item-table tbody").html("");
+    for (var i = 0;i<100;i++){
+        $("#sale-item-table tbody").append("<tr><td></td><td></td><td></td><td></td><td></td></tr>");
+    }
+}
+//添加销售明细
+function addSaleItem(barcode,name,price){
+    var str = "<tr><td>"+barcode+"</td><td>"+name+"</td><td><input type='number' min='0' value='1' onchange='subTotal(this,this.value)'></td><td>"+(price-0).toFixed(2)+"</td><td>"+(price-0).toFixed(2)+"</td></tr>"
+    $("#sale-item-table tbody").prepend(str);
+}
+//改变数量，小计变化
+function subTotal(eventObj,quantity){
+   var price = $(eventObj).parent().siblings().eq(3).text() - 0;
+   var subTotal = quantity * price;
+   $(eventObj).parent().siblings().eq(3).html(subTotal.toFixed(2))//.toFixed(2)保留小数点两位
+}
+function calSaleAmount(){
+    
+}
 //列出所有的商品，dataTable展示
 function searchGoods() {
     var barcode = $("#search-goods-barcode").val();
@@ -89,12 +111,12 @@ function searchGoods() {
         }
     });
 }
-//辅助getAllGoods()方法，dataTable
+//辅助searchGoods()方法，dataTable
 function goodsDataTable(data) {
     var goodsTable = $('#goods-table')
             .DataTable(
                     {
-                        destroy : true,
+                        "destroy" : true,
                         "bAutoWidth" : false,
                         "aoColumnDefs" : [ {
                                                 "bSearchable" : false,
@@ -109,11 +131,14 @@ function goodsDataTable(data) {
                         data : data,
                         // 使用对象数组，一定要配置columns，告诉 DataTables 每列对应的属性
                         columns : [ {
-                            data : 'barcode'
+                            "className": 'goods-barcode',
+                            "data" : 'barcode'
                         }, {
-                            data : 'name'
+                            "className": 'goods-name',
+                            "data" : 'name'
                         }, {
-                            data : 'price'
+                            "className": 'goods-price',
+                            "data" : 'price'
                         }, {
                             data : 'id'
                         },{
@@ -130,5 +155,17 @@ function goodsDataTable(data) {
                         "fnRowCallback" : function(nRow, aaData, iDisplayIndex,
                                 iDisplayIndexFull) {
                         },
-                    });
+     });    
+    var flag = 0;
+    //添加双击事件
+    $('#goods-table tbody').on('dblclick.dt', 'tr' , function () {
+        var barcode = $(this).find(".goods-barcode").text();
+        var name = $(this).find(".goods-name").text();
+        var price = $(this).find(".goods-price").text();
+        if(flag==0){
+            addSaleItem(barcode,name,price);
+        }
+        $("#goodsModal").modal('hide');//关闭模态库
+        flag++;
+    } );
 }
